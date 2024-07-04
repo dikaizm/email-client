6e3212c3921602e94d7a66430c5c3030bd18b4d6
Dika <dikamaah@gmail.com>
2024-07-04 19:29:57 +0700

    chore: hide secret

--- config/settings.py
+++ config/settings.py
@@ -10,9 +10,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5475(xq9zgo@%-35v^wvuz*qabk)v21p=zxoq&f=94n88usz*!'
 
-GOOGLE_OAUTH2_CLIENT_ID = '590573173268-jekrpf6vqe21ks5na2rbsps0q5nt34kk.apps.googleusercontent.com'
-GOOGLE_OAUTH2_CLIENT_SECRET = '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'
-GOOGLE_OAUTH2_PROJECT_ID = 'ayu-email-client'
+# GOOGLE_OAUTH2_CLIENT_ID = '590573173268-jekrpf6vqe21ks5na2rbsps0q5nt34kk.apps.googleusercontent.com'
+# GOOGLE_OAUTH2_CLIENT_SECRET = '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'
+# GOOGLE_OAUTH2_PROJECT_ID = 'ayu-email-client'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
